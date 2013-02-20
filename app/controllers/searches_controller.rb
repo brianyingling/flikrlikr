f8981baf2d0dac900eec83a6ada3d730bb9f65bd
Brian Yingling <brian_yingling@yahoo.com>
2013-02-20 08:37:36 -0500

    refactored search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -42,43 +42,24 @@ class SearchesController < ApplicationController
   end
 
   def show
-    # FlickRaw.api_key = "756092d22ac3734e2746938aeb7b2488"
-    # FlickRaw.shared_secret = "c799cf3e0a721081"
-    # FlickRaw.api_key = ENV['FLICKR_API_KEY']
-    # FlickRaw.shared_secret = ENV['FLICKR_SHARED_SECRET']
     @search = Search.find(params[:id])
     photos = ''
     url = ''
     @photo = Photo.create
     if @search.text != 'random'
       photos = flickr.photos.search(:text=>@search.text,:extras=>'tags',:accuracy=>3)
-      #filtered_photos = photos.reject {|photo| Photo.exists?(:url=>build_urls(photo))}
-      #chosen_photo = filtered_photos.shuffle.first
-      chosen_photo = format_photos(photos).shuffle.first
-      @photo.url = build_urls(chosen_photo)
-      @photo.title = chosen_photo.title
-      @tags = chosen_photo.tags.split(' ')
-      #photos = filtered_photos
     else
       photos = flickr.photos.getRecent(:extras=>'tags,url_c')
-      filtered_photos = format_photos(photos)
-      #filtered_photos = photos.reject {|photo| Photo.exists?(:url=>build_urls(photo))}
-      #url = build_urls(photos)
-      #@photo.url = url[0]
-      #@photo.url = filtered_photos[0].url_c
-      @photo.url = filtered_photos[0].url_c || build_urls(filtered_photos[0])
-      @photo.title = filtered_photos.first.title
-      @tags = filtered_photos.first.tags.split(' ')
-  end
-    # @photo.title = photos.first.title
-    # @tags = photos.first.tags.split(' ')
+    end
+    chosen_photo = format_photos(photos).shuffle.first
+    @photo.url = build_urls(chosen_photo)
+    @photo.title = chosen_photo.title
+    @tags = chosen_photo.tags.split(' ')
     @photo.save
   end
 
   def destroy
   end
-
-
 end
 
 
